6fb276b2c3ea987fb1929d822ea7934080bb5239
waspryo <wasp2123321@gmail.com>
2019-07-22 13:55:40 +0900

    get response

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-    console.log('Someone did access the server!');
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.end('This is the response!');
 });
 
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening for requests now');
+})
